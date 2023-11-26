ef08fab08c023a82d8824f01df2bc7d02428ffd8
Rémy Marquis <remy.marquis@gmail.com>
2023-11-26 21:02:40 +0100

    Add Lazy update shortcut on dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,12 +1,20 @@
 return {
-<<<<<<< HEAD
-  'nvimdev/dashboard-nvim',
-  dependencies = {{ 'nvim-tree/nvim-web-devicons' }},
-  event = 'VimEnter',
-  config = function()
-    require('dashboard').setup {
-      -- config
-    }
-  end,
+    'nvimdev/dashboard-nvim',
+    dependencies = {{'nvim-tree/nvim-web-devicons' }},
+    event = 'VimEnter',
+    config = function()
+        require('dashboard').setup {
+            -- config
+            theme = 'hyper',
+            config = {
+                week_header = {
+                    enable = true,
+                },
+                shortcut = {
+                    { icon = '📦 ', desc = 'Update', group = '@property', action = 'Lazy update', key = 'u' },
+                },
+            },
+        }
+    end,
 }
 
