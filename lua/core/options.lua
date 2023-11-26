ba115751ab6d60677c3edab1b1b26173f0cda2c5
Rémy Marquis <remy.marquis@gmail.com>
2023-11-26 20:09:33 +0100

    Add initial lazy structure

---
+++ lua/core/options.lua
@@ -0,0 +1,12 @@
+-- Set options
+-- See :help vim.om :set all, :options
+
+-- tabs and indentation
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+-- appearance
+vim.opt.number = true
+
