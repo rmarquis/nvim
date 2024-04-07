2b9161d328f208f828f894c989a70234f40ff8cb
Rémy Marquis <remy.marquis@gmail.com>
2024-04-07 22:51:12 +0200

    Enable tree-sitter autoinstall

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -15,7 +15,7 @@ return {
                 "zig",
             },
             sync_install = false,
-            auto_install = false,
+            auto_install = true,
             highlight = {
                 enable = true,
                 additional_vim_regex_highlighting = false,
