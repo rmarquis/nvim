68894421b66cc6a72d7b463266ae847cc034dcb3
Rémy Marquis <remy.marquis@heig-vd.ch>
2025-02-11 13:19:48 +0100

    Add yank highlight autocmd

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,2 +1,10 @@
 -- Set autocomds
 -- See :help autocmd
+vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = "YankHighlight",
+    callback = function()
+        vim.highlight.on_yank({ timeout = 300 })
+    end,
+})
+
