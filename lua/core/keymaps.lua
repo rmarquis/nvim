92c73df72977aaad2f922368c2acad1d17ab2aa6
Rémy Marquis <remy.marquis@gmail.com>
2025-03-16 16:27:06 +0100

    Add core keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,1 +1,9 @@
 -- [[ keymaps ]]
+-- see `:help vim.keymap.set()`
+
+-- for conciseness
+local opts = { noremap = true, silent = true }
+
+-- backspace
+vim.keymap.set("i", "<S-Tab>", "<BS>", opts)
+
