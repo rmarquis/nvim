512bda68f2f46c381c52e098ab09a2194b36be9d
Rémy Marquis <remy.marquis@gmail.com>
2025-03-16 16:47:56 +0100

    Add lazy plugins support

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -11,7 +11,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {} -- "plugins"
+local plugins = "plugins"
 local opts = {}
 
 require("lazy").setup(plugins, opts)
