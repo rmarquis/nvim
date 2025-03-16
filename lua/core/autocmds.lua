a0fa0d926e5eea57c7ef25c8002d0abf2693366e
Rémy Marquis <remy.marquis@gmail.com>
2025-03-16 16:29:53 +0100

    Add core highlight on yank autocommand

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,1 +1,13 @@
 -- [[ autocommands ]]
+-- see `:help lua-guide-autocommands`
+
+-- Highlight when yanking text
+-- see `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking text',
+  group = vim.api.nvim_create_augroup('YankHighlight', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
