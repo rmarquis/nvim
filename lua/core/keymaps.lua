6edf2a20fdeab97a399d6dec95d815c91615d7c3
Rémy Marquis <remy.marquis@gmail.com>
2025-03-31 17:38:20 +0200

    Add core keymap split navigation shortcut

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -28,3 +28,9 @@ vim.keymap.set("n", "<leader>q", "<cmd>qa<CR>", { desc = "Quit" })
 -- lazy
 vim.keymap.set("n", "<leader>l", "<cmd>Lazy<CR>", { desc = "Lazy" })
 
+-- split navigation
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
