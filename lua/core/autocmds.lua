0045e7c154c095bc0637e6e978b56d91a05b2df6
Rémy Marquis <remy.marquis@gmail.com>
2025-09-22 23:19:17 +0200

    Clear terminal when exiting

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,13 +1,18 @@
 -- [[ autocommands ]]
 -- see `:help lua-guide-autocommands`
 
--- Highlight when yanking text
--- see `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking text',
-  group = vim.api.nvim_create_augroup('YankHighlight', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+-- Clear terminal when exiting Neovim
+vim.api.nvim_create_autocmd("VimLeave", {
+    pattern = "*",
+    command = "!clear",
 })
 
+-- Highlight when yanking text
+-- see `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking text",
+    group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
